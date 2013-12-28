1d92b778997fb70051dcf056e15cdffcb0810730
Alexandru Rosianu <rosian_alex@yahoo.com>
2013-12-28 17:48:37 +0200

    Merged stylesheets into @import's, finished layout for navbar, added color palette

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,19 +2,6 @@
 
 angular.module('catticsApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    },
-    {
-      'title': 'Leaderboard',
-      'link': '/leaderboard'
-    },
-    {
-      'title': 'Sign in',
-      'link': '/signin'
-    }];
-
     $scope.isActive = function(route) {
       return route === $location.path();
     };
