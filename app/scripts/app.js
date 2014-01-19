a779bb88ad7e2a54973f3c235d95ed90c07d50c9
Aluxian <rosian_alex@yahoo.com>
2014-01-19 10:19:31 +0200

    Added footer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,23 +4,23 @@ angular.module('catticsApp', [
   'ngRoute',
   'restangular'
 ])
-  // Set routes
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main'
-      })
-      .when('/explore/:what', {
-        templateUrl: 'partials/explore',
-        controller: 'ExploreCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5Mode(true);
-  })
+// Set routes
+.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/main'
+    })
+    .when('/explore/:what', {
+      templateUrl: 'partials/explore',
+      controller: 'ExploreCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+  $locationProvider.html5Mode(true);
+})
 
-  // Restangular config
-  .config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('/api');
-  });
+// Restangular config
+.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('/api');
+});
